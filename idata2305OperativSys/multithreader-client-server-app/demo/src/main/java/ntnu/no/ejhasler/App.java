974599890c5cce546ac8e1a068339aabadb26cd2
ejhasler <ejhasler@stud.ntnu.no>
2023-02-10 15:26:09 +0100

    Started on multithreading assignment Operating System

---
+++ idata2305OperativSys/multithreader-client-server-app/demo/src/main/java/ntnu/no/ejhasler/App.java
@@ -0,0 +1,13 @@
+package ntnu.no.ejhasler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
