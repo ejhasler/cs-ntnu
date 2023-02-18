7875b3c8e1a4a410ece9d361c146c199a4e55964
ejhasler <ejhasler@stud.ntnu.no>
2023-02-18 13:32:23 +0100

    Done assignment week02 AppDev

---
+++ idata2306AppDev/assignments/week02/demo/src/main/java/idata2306/demo/HelloController.java
@@ -0,0 +1,13 @@
+package idata2306.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String greeting() {
+        return "Hello World!";
+    }
+}
