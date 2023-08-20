bd5a5eb3eaddefb9f0ef6f519ce7837cbdc0835a
ejhasler <ejhasler@stud.ntnu.no>
2023-08-20 22:42:37 +0200

    Added project structure

--- idata2306AppDev/related-courses/spring-boot-course-java-brains/course-api/src/main/java/io/javabrains/springbootquickstart/courseapi/course/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package io.javabrains.springbootquickstart.courseapi.course;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CourseRepository extends CrudRepository<Course, String> {
-
-}
--- idata2306AppDev/related-courses/spring-boot-course-java-brains/course-api/src/main/java/io/javabrains/springbootquickstart/courseapi/topic/TopicRepository.java
+++
@@ -1,7 +0,0 @@
-package io.javabrains.springbootquickstart.courseapi.topic;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface TopicRepository extends CrudRepository<Topic, String> {
-
-}
